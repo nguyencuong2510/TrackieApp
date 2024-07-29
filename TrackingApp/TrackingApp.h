5874b8c5af7b7c942208fa3aab089265db4ff4e7
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2024-07-29 22:49:18 +0700

    Initial Commit

---
+++ TrackingApp/TrackingApp.h
@@ -0,0 +1,18 @@
+//
+//  TrackingApp.h
+//  TrackingApp
+//
+//  Created by nguyen cuong on 29/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrackingApp.
+FOUNDATION_EXPORT double TrackingAppVersionNumber;
+
+//! Project version string for TrackingApp.
+FOUNDATION_EXPORT const unsigned char TrackingAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrackingApp/PublicHeader.h>
+
+
